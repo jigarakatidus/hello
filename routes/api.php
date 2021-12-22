7fd9528c1310de6d5cd5c647ab0a90edf7f1a574
Jigar Dhulla <jigar.d@endurance.com>
2021-12-22 16:54:41 +0530

    Enable Sanctum for SPA Authentication

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('hello', \App\Http\Controllers\HelloController::class);
+Route::middleware('auth:sanctum')->resource('hello', \App\Http\Controllers\HelloController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
